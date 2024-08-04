43325b297cf8c2a71b1e01d3c1c37cbbcf7665f2
Mark Bauer <bauer.markedward@gmail.com>
2024-08-04 16:52:49 -0400

    initial commit

---
+++ export.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import duckdb
+from datetime import datetime
+import time
+
+export_history = 'export-history.txt'
+
+limit = 3000000
+rows = limit
+offset = 0
+
+api_base = "https://data.ny.gov/resource/uhf3-t34z.csv"
+con = duckdb.connect("subway-trips.db", config={"allow_unsigned_extensions": "true"})
+
+while rows == limit:
+    
+    api_path = f"{api_base}?$limit={limit}&$offset={offset}"
+    timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    
+    with open(export_history, 'a') as file:
+        file.write(f"{timestamp},{api_path}\n")
+    
+    try:
+        df = pd.read_csv(api_path)
+        rows = df.shape[0]
+        
+        if offset == 0:
+            con.sql("CREATE TABLE trips AS SELECT * FROM df")
+        else:
+            con.sql("INSERT INTO trips SELECT * FROM df")
+            
+        offset += limit
+
+        time.sleep(3)
+        
+    except Exception as e:
+        with open(export_history, 'w') as file:
+            file.write(f"Error reading data: {e}\n")
+        break
+
+con.close()        
